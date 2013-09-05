7936e8aa93075ca8c3ccc9829080579242a8d1e6
mediahack <code@mediahack.com>
2013-09-05 01:01:45 -0500

    Fixed issue with usage stats displaying newline \n rather than an actual newline. Was an issue of the text sections with \n being encapsulated in single quotes instead of double.

--- app/core/common.php
+++ app/core/common.php
@@ -16,23 +16,23 @@ class Common{
 
 	/**
 	* This function vomits out the value of the subject in a readable form.
-	* 
+	*
 	* @param 	Mixed 	$obj 	The subject we wish to inspect
 	**/
 	static public function dump($obj){
 		echo '<pre>'; 
 		print_r($obj);
 		exit();
-	}	
+	}
 
 	/**
 	* Print our the memory being used by the current request
 	**/
 	static public function usage(){
-		echo '\n<b>Memory Usage</b><pre>' . 
-		number_format(memory_get_usage() - START_MEMORY_USAGE) . ' bytes\n'.
-		number_format(memory_get_usage()) . ' bytes (process)\n' .
-		number_format(memory_get_peak_usage(TRUE)) . ' bytes (process peak)\n' .
+		echo "\n<b>Memory Usage</b><pre>" .
+		number_format(memory_get_usage() - START_MEMORY_USAGE) . " bytes\n".
+		number_format(memory_get_usage()) . " bytes (process)\n" .
+		number_format(memory_get_peak_usage(TRUE)) . " bytes (process peak)\n" .
 		'</pre><b>Execution Time</b><pre>'.
 		round((microtime(true) - START_TIME), 5) . ' seconds</pre>';
 	}
